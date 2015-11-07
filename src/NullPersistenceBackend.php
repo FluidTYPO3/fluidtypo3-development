eec9edf5fc763a30d4cb9128aa897646c3d3bd36
Claus Due <claus@namelesscoder.net>
2015-11-07 19:47:53 +0100

    [TASK] Implement required PersistenceBackendInterface

--- src/NullPersistenceBackend.php
+++ src/NullPersistenceBackend.php
@@ -10,11 +10,60 @@ namespace FluidTYPO3\Development;
 
 use TYPO3\CMS\Extbase\Persistence\Generic\Storage\Typo3DbBackend;
 use TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface;
+use TYPO3\CMS\Extbase\Persistence\Generic\BackendInterface;
 
 /**
  * Class NullPersistenceBackend
  */
-class NullPersistenceBackend extends Typo3DbBackend {
+class NullPersistenceBackend extends Typo3DbBackend implements BackendInterface {
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Persistence\ObjectStorage $objects
+	 * @return void
+	 */
+	public function setAggregateRootObjects(\TYPO3\CMS\Extbase\Persistence\ObjectStorage $objects) {
+
+	}
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Persistence\ObjectStorage $entities
+	 * @return void
+	 */
+	public function setDeletedEntities(\TYPO3\CMS\Extbase\Persistence\ObjectStorage $entities) {
+
+	}
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Persistence\ObjectStorage $entities
+	 * @return void
+	 */
+	public function setChangedEntities(\TYPO3\CMS\Extbase\Persistence\ObjectStorage $entities) {
+
+	}
+
+	/**
+	 * @return void
+	 */
+	public function commit() {
+
+	}
+
+	/**
+	 * @param string $identifier
+	 * @param string $className
+	 * @return void
+	 */
+	public function getObjectByIdentifier($identifier, $className) {
+
+	}
+
+	/**
+	 * @param object $object
+	 * @return boolean
+	 */
+	public function isNewObject($object) {
+
+	}
 
 	/**
 	 * @param PersistenceManagerInterface $manager
