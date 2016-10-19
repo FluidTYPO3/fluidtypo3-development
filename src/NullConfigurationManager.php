c6bf6f3e9bb1de3b0199145073d434854a4648ca
Claus Due <claus@namelesscoder.net>
2016-10-19 03:54:42 +0200

    [BUGFIX] Fix warning from NullConfigurationManager

--- src/NullConfigurationManager.php
+++ src/NullConfigurationManager.php
@@ -1,7 +1,7 @@
 <?php
 namespace FluidTYPO3\Development;
 
-use TYPO3\CMS\Extbase\Configuration\BackendConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\AbstractConfigurationManager;
 use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
@@ -9,7 +9,7 @@ use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 /**
  * Class DummyConfigurationManager
  */
-class NullConfigurationManager extends BackendConfigurationManager implements ConfigurationManagerInterface {
+class NullConfigurationManager implements ConfigurationManagerInterface {
 
 	/**
 	 * @param string $type
@@ -18,15 +18,7 @@ class NullConfigurationManager extends BackendConfigurationManager implements Co
 	 * @return array
 	 */
 	public function getConfiguration($type, $extensionName = NULL, $pluginName = NULL) {
-		return array(
-			'config' => array(
-				'tx_extbase' => array(
-					'features' => array(
-						'rewrittenPropertyMapper' => TRUE
-					)
-				)
-			)
-		);
+		return $this->getTypoScriptSetup();
 	}
 
 	/**
@@ -43,5 +35,68 @@ class NullConfigurationManager extends BackendConfigurationManager implements Co
 	public function getContentObject() {
 		return new ContentObjectRenderer();
 	}
+
+    /**
+     * @param array $frameworkConfiguration
+     * @return array
+     */
+    protected function getContextSpecificFrameworkConfiguration(array $frameworkConfiguration) {
+        return array();
+    }
+
+    /**
+     * @return array
+     */
+    public function getTypoScriptSetup() {
+        return array(
+            'config' => array(
+                'tx_extbase' => array(
+                    'features' => array(
+                        'rewrittenPropertyMapper' => TRUE
+                    )
+                )
+            )
+        );
+    }
+
+    /**
+     * @param string $extensionName
+     * @param string|NULL $pluginName
+     */
+    protected function getPluginConfiguration($extensionName, $pluginName = null) {
+        return array();
+    }
+
+    /**
+     * @param string $extensionName
+     * @param string $pluginName
+     * @return array
+     */
+    protected function getSwitchableControllerActions($extensionName, $pluginName) {
+        return array();
+    }
+
+    /**
+     * @param string $storagePid
+     * @param integer $recursionDepth
+     * @return array
+     */
+    protected function getRecursiveStoragePids($storagePid, $recursionDepth = 0) {
+        return array();
+    }
+
+    /**
+     * @param ContentObjectRenderer|NULL $contentObject
+     * @return void
+     */
+    public function setContentObject(\TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer $contentObject = NULL) {
+    }
+
+    /**
+     * @param array $configuration
+     * @return void
+     */
+    public function setConfiguration(array $configuration = array()) {
+    }
 
 }
